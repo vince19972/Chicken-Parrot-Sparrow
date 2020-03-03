6f3ab2482e07e54df67798fb09f9a38a06252c04
vince19972 <vince19972@gmail.com>
2020-03-03 18:45:45 -0500

    [chicken] enhance produce, add inner bar

--- src/components/comps/Chicken/Produce.vue
+++ src/components/comps/Chicken/Produce.vue
@@ -17,8 +17,9 @@ export default class Produce extends Vue {}
 <style scoped lang="scss">
 @import "@/assets/styles/components/ChickenGrid.scss";
 
-$bar-duration: 1s;
+$bar-duration: 1.5s;
 $input-bar-length: 100%;
+$inner-bar-height: 10vh;
 
 .produce {
   height: 100%;
@@ -30,10 +31,20 @@ $input-bar-length: 100%;
   background-color: black;
 }
 .lead-input.-bar {
+  position: relative;
   height: $input-bar-length;
   width: calc(#{$gutter-v});
   margin-bottom: calc(#{$v-gutter-v} / 6);
   border-radius: 0 0 calc(#{$gutter-v}* 0.5) calc(#{$gutter-v}* 0.5);
+}
+.lead-input.-bar:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: calc((100% - 15%) / 2);
+  width: 15%;
+  height: $inner-bar-height;
+  background-color: white;
 }
 
 .lead {
@@ -51,7 +62,10 @@ $input-bar-length: 100%;
   100% {
     transform: translate3d(0, -100%, 0);
   }
-  50% {
+  30% {
+    transform: translate3d(0, -1%, 0);
+  }
+  80% {
     transform: translate3d(0, 0, 0);
   }
 }
@@ -60,21 +74,50 @@ $input-bar-length: 100%;
   100% {
     letter-spacing: normal;
   }
-  50% {
-    letter-spacing: -0.2rem;
+  30% {
+    letter-spacing: -1.9rem;
+  }
+  80% {
+    letter-spacing: -2rem;
+  }
+}
+@keyframes moveInnerBar {
+  0%,
+  100% {
+    opacity: 0;
+    top: -100%;
+  }
+  60% {
+    opacity: 1;
+    top: -100%;
+  }
+  79% {
+    opacity: 1;
+    top: calc(100% - #{$inner-bar-height});
+  }
+  80% {
+    opacity: 1;
+    top: 100vh;
+  }
+  81% {
+    opacity: 0;
   }
 }
 
-.lead-input {
-  animation-name: moveInputBar;
-  animation-timing-function: ease-in-out;
+.lead-input,
+.lead-text,
+.lead-input.-bar:after {
+  // animation-timing-function: ease-in-out;
   animation-duration: $bar-duration;
   animation-iteration-count: infinite;
 }
+.lead-input {
+  animation-name: moveInputBar;
+}
 .lead-text {
   animation-name: moveLeadText;
-  animation-timing-function: ease-in-out;
-  animation-duration: $bar-duration;
-  animation-iteration-count: infinite;
+}
+.lead-input.-bar:after {
+  animation-name: moveInnerBar;
 }
 </style>
