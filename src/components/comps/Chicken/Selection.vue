4aa0b060f891c673049c0464daa94840fc3e49e0
vince19972 <vince19972@gmail.com>
2020-03-04 16:25:58 -0500

    [chicken] selection - create calculated layout

--- src/components/comps/Chicken/Selection.vue
+++ src/components/comps/Chicken/Selection.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="selection">
-    <div class="text-group" v-for="x in 10" :key="x">
-      <p class="text" v-for="n in 20" :key="n">CHICKEN</p>
+    <div class="text-group">
+      <div class="text-row" v-for="r in textCount.row" :key="r + 'row'">
+        <p class="text" v-for="c in textCount.col" :key="c + 'col'">CHICKEN</p>
+      </div>
     </div>
+    <p class="text -hidden" ref="textHidden">CHICKEN</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { WindowSize as WindowSizeState } from "@/store/types/browser";
 
 @Component
-export default class Selection extends Vue {}
+export default class Selection extends Vue {
+  get windowSize(): WindowSizeState {
+    return this.$store.getters["browser/windowSize"];
+  }
+  get textCount() {
+    return {
+      row: Math.ceil(this.windowSize.height / this.textValue.height),
+      col: Math.ceil(this.windowSize.width / this.textValue.width)
+    };
+  }
+  textValue = {
+    width: this.windowSize.width,
+    height: this.windowSize.height
+  };
+
+  mounted() {
+    const text = this.$refs.textHidden as HTMLElement;
+    this.textValue.width = text.offsetWidth;
+    this.textValue.height = text.offsetHeight;
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -21,7 +45,17 @@ export default class Selection extends Vue {}
   display: flex;
 }
 
+.text-row {
+  display: flex;
+}
+
 .text {
-  // font-size: $f-size-sub;
+  &.-hidden {
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+  }
 }
 </style>
