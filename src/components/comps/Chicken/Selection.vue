a336d09fd896216ab9b9a1667e80fadc485f7886
vince19972 <vince19972@gmail.com>
2020-03-05 12:07:17 -0500

    [chicken] selection - egg animation

--- src/components/comps/Chicken/Selection.vue
+++ src/components/comps/Chicken/Selection.vue
@@ -2,9 +2,9 @@
   <div class="selection">
     <div class="text-group">
       <div class="text-row" v-for="r in textCount.row" :key="r + 'row'">
-        <p class="text" v-for="c in textCount.col" :key="c + 'col'">
-          {{ textType(c) }}
-        </p>
+        <div class="text-col" v-for="c in textCount.col" :key="c + 'col'">
+          <p class="text-col__text">{{ textType(c) }}</p>
+        </div>
       </div>
     </div>
     <p class="text -hidden" ref="textHidden">CHICKEN</p>
@@ -17,6 +17,7 @@ import { WindowSize as WindowSizeState } from "@/store/types/browser";
 
 @Component
 export default class Selection extends Vue {
+  // computed setters
   get windowSize(): WindowSizeState {
     return this.$store.getters["browser/windowSize"];
   }
@@ -26,15 +27,20 @@ export default class Selection extends Vue {
       col: Math.ceil(this.windowSize.width / this.textValue.width) * 2
     };
   }
+
+  // methods
   textType(index: number) {
     return index % 2 ? "CHICKEN" : "CHIcKEN";
   }
 
+  // data
   textValue = {
     width: this.windowSize.width,
     height: this.windowSize.height
   };
+  eggAnimationTime = 1;
 
+  // life cycle
   mounted() {
     const text = this.$refs.textHidden as HTMLElement;
     this.textValue.width = text.offsetWidth;
@@ -55,26 +61,86 @@ export default class Selection extends Vue {
   display: flex;
 
   &:nth-child(1n) {
-    transform: translate3d(-5vw, 0, 0);
-  }
-  &:nth-child(2n) {
     transform: translate3d(-10vw, 0, 0);
   }
-  &:nth-child(3n) {
+  &:nth-child(2n) {
     transform: translate3d(-20vw, 0, 0);
-  }
-  &:nth-child(5n) {
-    transform: translate3d(-30vw, 0, 0);
   }
 }
 
-.text {
-  &.-hidden {
-    opacity: 0;
+.text.-hidden {
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+}
+
+.text-col {
+  position: relative;
+  margin-right: calc(#{$v-gutter} / 2);
+
+  &:after {
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
-    pointer-events: none;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    border-radius: 100%;
   }
+}
+
+/*
+** animation
+*/
+
+$egg-duration: 2s;
+
+@keyframes scaleEgg {
+  0% {
+    transform: scale3d(1, 1, 1);
+  }
+  50% {
+    transform: scale3d(1, 2, 1);
+  }
+  51%,
+  100% {
+    transform: scale3d(1, 1, 1);
+  }
+}
+@keyframes colorEgg {
+  0%,
+  48% {
+    background-color: #f0f0f0;
+  }
+  49%,
+  100% {
+    background-color: transparent;
+  }
+}
+@keyframes showText {
+  0%,
+  48%,
+  100% {
+    opacity: 0;
+  }
+  49%,
+  99% {
+    opacity: 1;
+  }
+}
+
+.text-col:after,
+.text-col__text {
+  animation-duration: $egg-duration;
+  animation-iteration-count: infinite;
+}
+.text-col:after {
+  animation-name: scaleEgg, colorEgg;
+}
+.text-col__text {
+  animation-name: showText;
 }
 </style>
