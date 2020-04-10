ee922e5d6181d61932ffce5b04585e6821cb46d3
vince19972 <vince19972@gmail.com>
2020-04-09 23:58:37 -0400

    formatter working here

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,15 @@ import GridSection from "./components/sections/GridSection.vue";
 @Component({
   components: {
     OpeningSection,
-    GridSection
-  }
+    GridSection,
+  },
 })
 export default class App extends Vue {
   created() {
     const { resizeEvent, scrollEvent, mouseEvent } = browserEvent(this.$store);
     window.addEventListener("resize", () => resizeEvent(null));
     window.addEventListener("scroll", () => scrollEvent(null));
-    window.addEventListener("mousemove", e =>
+    window.addEventListener("mousemove", (e) =>
       mouseEvent({ x: e.clientX, y: e.clientY })
     );
   }
@@ -31,7 +31,7 @@ export default class App extends Vue {
     const { resizeEvent, scrollEvent, mouseEvent } = browserEvent(this.$store);
     window.removeEventListener("resize", () => resizeEvent(null));
     window.removeEventListener("scroll", () => scrollEvent(null));
-    window.removeEventListener("mousemove", e =>
+    window.removeEventListener("mousemove", (e) =>
       mouseEvent({ x: e.clientX, y: e.clientY })
     );
   }
--- src/components/partials/ChickenGrid.vue
+++ src/components/partials/ChickenGrid.vue
@@ -21,18 +21,18 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Produce from "@/components/comps/Chicken/Produce.vue";
-import Selection from "@/components/comps/Chicken/Selection.vue";
-import Stack from "@/components/comps/Chicken/Stack.vue";
-import Slaughter from "@/components/comps/Chicken/Slaughter.vue";
+import Produce from "@/components/comps/chicken/Produce.vue";
+import Selection from "@/components/comps/chicken/Selection.vue";
+import Stack from "@/components/comps/chicken/Stack.vue";
+import Slaughter from "@/components/comps/chicken/Slaughter.vue";
 
 @Component({
   components: {
     Produce,
     Selection,
     Stack,
-    Slaughter
-  }
+    Slaughter,
+  },
 })
 export default class ChickenGrid extends Vue {}
 </script>
