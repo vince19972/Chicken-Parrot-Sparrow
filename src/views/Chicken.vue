170dbf1780a5b59e3ba3589a6c5751aa261985e0
vince19972 <vince19972@gmail.com>
2020-04-12 21:38:42 -0400

    [chicken] add chicken route

---
+++ src/views/Chicken.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chicken">
+    this is chicken page
+    <!-- <OpeningSection /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import OpeningSection from "@/components/sections/OpeningSection.vue";
+
+@Component({
+  components: {
+    OpeningSection
+  }
+})
+export default class Chicken extends Vue {
+  components: {
+    OpeningSection;
+  };
+}
+</script>
