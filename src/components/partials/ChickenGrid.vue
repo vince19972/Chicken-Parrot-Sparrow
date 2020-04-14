09c10907abf662edc4a159c82e241cbd73d98da9
vince19972 <vince19972@gmail.com>
2020-04-14 13:37:05 -0400

    [GridSection] merge ChickenGrid style into GridSec

--- src/components/partials/ChickenGrid.vue
+++ src/components/partials/ChickenGrid.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="chicken-grid" :style="[transformStyle]">
-    <div class="grid-v -full-height">
+  <div class="chicken-grid -grid-base" :style="[transformStyle]">
+    <div class="-grid-vertical">
       <Produce v-for="n in 12" :key="n" />
     </div>
-    <div class="grid -full-height">
-      <div class="grid__container -grid-">
+    <div class="-grid-horizontal">
+      <div class="-full-height">
         <Selection />
       </div>
     </div>
-    <div class="grid -full-height">
-      <div class="grid__container -grid-">
+    <div class="-grid-horizontal">
+      <div class="-full-height">
         <Stack />
       </div>
     </div>
-    <div class="grid-v -full-height">
+    <div class="-grid-vertical">
       <Slaughter />
     </div>
   </div>
@@ -53,63 +53,4 @@ export default class ChickenGrid extends Vue {
 
 <style scoped lang="scss">
 @import "@/assets/styles/sections/GridSection.scss";
-@import "@/assets/styles/components/ChickenGrid.scss";
-
-.chicken-grid {
-  overflow: hidden;
-
-  transition: transform 0.5s ease-in-out;
-}
-
-.grid {
-  font-size: $size-root;
-}
-.grid__container {
-  height: 100%;
-
-  &.-grid {
-    position: relative;
-    background: repeating-linear-gradient(
-      to bottom,
-      red 0 0.1vh,
-      transparent 0 $gutter,
-      green $gutter calc(#{$gutter} + 0.1vh),
-      transparent $gutter calc(#{$size-root} - #{$gutter}),
-      green calc(#{$size-root} - #{$gutter})
-        calc(#{$size-root} - #{$gutter} + 0.1vh),
-      transparent calc(#{$size-root} - #{$gutter}) $size-root
-    );
-  }
-  &.-grid:after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: repeating-linear-gradient(
-      to bottom,
-      transparent 0 calc(#{$gutter} + #{$row}),
-      purple calc(#{$gutter} + #{$row}) calc(#{$gutter} + #{$row} + 0.1vh),
-      transparent calc(#{$gutter} + #{$row}) calc(#{$gutter} * 2 + #{$row}),
-      purple calc(#{$gutter} * 2 + #{$row})
-        calc(#{$gutter} * 2 + #{$row} + 0.1vh),
-      transparent calc(#{$gutter} * 2 + #{$row}) $size-root
-    );
-  }
-}
-
-.grid-v {
-  width: $container-width;
-  margin: 0 auto;
-  font-size: $size-root-v;
-  display: flex;
-  justify-content: space-between;
-}
-
-.-full-height {
-  max-height: 100vh;
-  height: 100vh;
-  overflow: hidden;
-}
 </style>
