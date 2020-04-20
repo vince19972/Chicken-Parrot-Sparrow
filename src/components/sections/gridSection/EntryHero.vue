27de042d27b662b2d6cb3bf75ccffddd980f90c5
vince19972 <vince19972@gmail.com>
2020-04-20 00:55:59 -0400

    [EntryHero] revise layout

--- src/components/sections/gridSection/EntryHero.vue
+++ src/components/sections/gridSection/EntryHero.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="entry -full-height -full-width -f-main" ref="entry">
-    <div class="entry__intro -full-height -flex-column">
+  <div
+    :class="[
+      'entry -full-height -full-width -f-main -flex-column-between',
+      getEntryClasses
+    ]"
+    ref="entry"
+  >
+    <div class="entry__intro">
+      <p class="entry__intro-text">
+        the journery of <br /><span class="-keyword">chicken</span> <br />
+        being objectified as <br /><span class="-keyword">food</span>.
+      </p>
+    </div>
+    <div class="entry__sub">
+      <p class="entry__sub-note">
+        * <br />
+        following section DOSE NOT contain any bloody or violent content.
+      </p>
+    </div>
+    <!-- <div class="entry__intro -full-height -flex-column">
       <div class="entry__intro-top">
         <p class="entry__intro-text">The journey of :</p>
       </div>
@@ -25,9 +43,14 @@
           </p>
         </div>
       </div>
-    </div>
-    <button class="entry__cta -f-main" @click="onBtnClicked">
-      click to enter
+    </div> -->
+    <button
+      class="entry__cta -f-main"
+      @click="onBtnClicked"
+      @mouseenter="onBtnEntered"
+      @mouseleave="onBtnLeft"
+    >
+      <span>click to enter</span>
     </button>
   </div>
 </template>
@@ -52,9 +75,23 @@ const sectionTitles = {
 export default class EntryHero extends Vue {
   @Prop() readonly contentType!: "chicken" | "sparrow" | "parrot";
 
+  // data
+  isBtnHovered = false;
+
+  // computed
+  get getEntryClasses() {
+    return this.isBtnHovered ? "-is-hovered" : "";
+  }
+
   // user events
   onBtnClicked() {
     this.$emit("onMouseClick");
+  }
+  onBtnEntered() {
+    this.isBtnHovered = true;
+  }
+  onBtnLeft() {
+    this.isBtnHovered = false;
   }
 }
 </script>
@@ -71,136 +108,162 @@ $offset-btm: 40px;
   position: relative;
   padding: 64px $offset-v $offset-btm $offset-v;
   color: white;
+  transition: all 0.3s;
 }
 
-.entry__intro-top {
-  margin: 0 0 40px 8px;
-}
 .entry__intro-text {
-  font-size: 1.5vw;
+  font-size: 5vw;
+  line-height: 5.5vw;
 }
 
-.hero {
-  flex-grow: 1;
-}
-.hero__text {
-  font-size: 5.5vw;
-  line-height: 6.5vw;
-
-  & .hero__text-keyword {
-    font-size: 6vw;
-    line-height: 6.5vw;
-  }
-}
-.hero__text.-note {
+.entry__sub-note {
+  position: relative;
   font-style: italic;
-  font-size: 16px;
-  line-height: initial;
-  max-width: 15%;
-  justify-content: flex-end;
-  align-items: flex-end;
-  margin-bottom: 24px;
-}
-.hero__text-btm {
-  flex-grow: 1;
-}
-.hero__text.-top {
-  & .hero__text-keyword {
-    display: inline-block;
-  }
-}
-.hero__text.-btm {
-  text-align: right;
-  justify-content: flex-end;
-  align-items: flex-end;
+  font-size: 18px;
+  line-height: 22px;
+  width: 20%;
 }
 
-.hero__text-span {
-  position: relative;
-  z-index: 5;
-  background-color: black;
+// .entry__intro-top {
+//   margin: 0 0 40px 8px;
+// }
+// .entry__intro-text {
+//   font-size: 1.5vw;
+// }
 
-  &.-front {
-    padding-right: 1.5vw;
-  }
-  &.-back {
-    padding-left: 1.5vw;
-  }
-  &.-btm {
-    padding-top: 1vw;
-  }
-}
+// .hero {
+//   flex-grow: 1;
+// }
+// .hero__text {
+//   font-size: 5.5vw;
+//   line-height: 6.5vw;
 
-.hero__line {
-  position: relative;
+//   & .hero__text-keyword {
+//     font-size: 6vw;
+//     line-height: 6.5vw;
+//   }
+// }
+// .hero__text.-note {
+//   font-style: italic;
+//   font-size: 16px;
+//   line-height: initial;
+//   max-width: 15%;
+//   justify-content: flex-end;
+//   align-items: flex-end;
+//   margin-bottom: 24px;
+// }
+// .hero__text-btm {
+//   flex-grow: 1;
+// }
+// .hero__text.-top {
+//   & .hero__text-keyword {
+//     display: inline-block;
+//   }
+// }
+// .hero__text.-btm {
+//   text-align: right;
+//   justify-content: flex-end;
+//   align-items: flex-end;
+// }
 
-  &:after {
-    content: "";
-    position: absolute;
-    z-index: 1;
-    background-color: white;
-  }
-  &.-horizontal:after {
-    top: calc(50% - 2px / 2);
-    left: 0;
-    width: 100%;
-    height: 2px;
-  }
-  &.-vertical:after {
-    right: 2vw;
-    top: 0;
-    height: 100%;
-    width: 2px;
-  }
-}
+// .hero__text-span {
+//   position: relative;
+//   z-index: 5;
+//   background-color: black;
+
+//   &.-front {
+//     padding-right: 1.5vw;
+//   }
+//   &.-back {
+//     padding-left: 1.5vw;
+//   }
+//   &.-btm {
+//     padding-top: 1vw;
+//   }
+// }
+
+// .hero__line {
+//   position: relative;
+
+//   &:after {
+//     content: "";
+//     position: absolute;
+//     z-index: 1;
+//     background-color: white;
+//   }
+//   &.-horizontal:after {
+//     top: calc(50% - 2px / 2);
+//     left: 0;
+//     width: 100%;
+//     height: 2px;
+//   }
+//   &.-vertical:after {
+//     right: 2vw;
+//     top: 0;
+//     height: 100%;
+//     width: 2px;
+//   }
+// }
 
 .entry__cta {
   position: absolute;
   z-index: 1;
-  left: 0;
-  bottom: $offset-btm;
+  right: 0;
+  bottom: 0;
   color: white;
   font-size: 32px;
-  margin-bottom: 3vw;
-  width: 100%;
+  text-align: right;
+
+  width: 80%;
+  height: 100%;
+
+  animation: pulseText 2s infinite;
+  animation-timing-function: ease-in-out;
+  transition: all 0.3s;
+
+  & span {
+    margin-right: calc(#{$offset-v} * 2.5);
+  }
 
   &:after {
     content: "";
     position: absolute;
-    left: calc(50% - 32px / 2);
-    bottom: -2.5vw;
-    width: 32px;
-    height: 32px;
+    right: calc(#{$offset-v} * 1.9);
+    bottom: calc(50% - 20px / 2);
+    width: 20px;
+    height: 20px;
     background: url("../../../assets/svgs/chevron-cursor.svg");
     background-repeat: no-repeat;
     background-size: contain;
+    transform: rotate(-90deg);
     transition: all 0.3s;
-    animation: pulse 1.5s ease-out;
-    animation-iteration-count: infinite;
   }
 
   @include hover {
+    animation: none;
+    transform: scale(1.1);
     &:after {
-      animation: none;
-      transform: translate3d(0, 12px, 0);
+      transform: rotate(-90deg) translate3d(0, 4px, 0);
       transition: all 0.3s;
     }
   }
 }
 
-@keyframes pulse {
-  0%,
+@keyframes pulseText {
+  0% {
+    opacity: 0.5;
+  }
+  70% {
+    opacity: 1;
+  }
   100% {
-    transform: translate3d(0, 0, 0);
+    opacity: 0.5;
   }
-  20% {
-    transform: translate3d(0, 8px, 0);
-  }
-  60% {
-    transform: translate3d(0, 4px, 0);
-  }
-  80% {
-    transform: translate3d(0, 8px, 0);
-  }
+}
+
+// states
+.entry.-is-hovered {
+  transform: translate3d(1%, 0, 0);
+  transition: all 0.3s;
 }
 </style>
