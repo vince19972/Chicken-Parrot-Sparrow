2b118b86192dc3b029409f08af51636b6d904b09
vince19972 <vince19972@gmail.com>
2020-04-21 12:01:10 -0400

    [readingList] build basic layout and functions

--- src/views/ReadingList.vue
+++ src/views/ReadingList.vue
@@ -1,14 +1,265 @@
 <template>
-  <div class="reading-list">
-    test
+  <div class="list -safe-container-full">
+    <div class="page-title">
+      <h1 class="page-title__text">Reading List.</h1>
+    </div>
+    <div class="list__container -flex">
+      <div class="list__side types" ref="menu">
+        <ul class="types__list">
+          <li
+            class="types__list-item -is-active"
+            data-type="general"
+            @click="onMenuClicked"
+          >
+            <button class="types__list-btn">general</button>
+          </li>
+          <li
+            class="types__list-item"
+            data-type="chicken"
+            @click="onMenuClicked"
+          >
+            <button class="types__list-btn">chicken</button>
+          </li>
+          <li
+            class="types__list-item"
+            data-type="parrot"
+            @click="onMenuClicked"
+          >
+            <button class="types__list-btn">parrot</button>
+          </li>
+          <li
+            class="types__list-item"
+            data-type="sparrow"
+            @click="onMenuClicked"
+          >
+            <button class="types__list-btn">sparrow</button>
+          </li>
+        </ul>
+      </div>
+      <div class="list__main readings" ref="readings">
+        <ul class="readings__list">
+          <transition-group name="fade" mode="out-in">
+            <li
+              v-for="(item, index) in activeReadingList"
+              :key="activeContent + index + item.url"
+              class="readings__list-item"
+            >
+              <div class="readings__list-item-info -flex">
+                <p class="__info-title">
+                  author: <span class="__info-text">{{ item.author }}</span>
+                </p>
+                <p class="__info-title">
+                  type: <span class="__info-text">{{ item.type }}</span>
+                </p>
+              </div>
+              <a
+                :href="item.url"
+                target="_blank"
+                class="readings__list-item-link"
+                >{{ item.title }}</a
+              >
+            </li>
+          </transition-group>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import readingLists from "@/assets/content/readingList.json";
+
+enum ContentTypes {
+  General = "general",
+  Chicken = "chicken",
+  Parrot = "parrot",
+  Sparrow = "sparrow"
+}
 
 @Component({
   name: "ReadingList"
 })
-export default class ReadingList extends Vue {}
+export default class ReadingList extends Vue {
+  // data
+  activeContent: ContentTypes = ContentTypes.General;
+  readingListsTypes = [
+    ContentTypes.General,
+    ContentTypes.Chicken,
+    ContentTypes.Parrot,
+    ContentTypes.Sparrow
+  ];
+
+  // computed
+  get activeReadingList() {
+    return readingLists[this.activeContent];
+  }
+
+  // user events
+  onMenuClicked(event) {
+    this.activeContent = event.currentTarget.dataset.type;
+    const items = document.querySelectorAll(".types__list-item");
+
+    items.forEach(item => {
+      if (item.dataset.type === this.activeContent) {
+        item.classList.add("-is-active");
+      } else {
+        item.classList.remove("-is-active");
+      }
+    });
+  }
+}
 </script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/_fonts.scss";
+@import "@/assets/styles/_helpers.scss";
+
+.page-title {
+  @include -f-main-b;
+  font-size: 64px;
+  margin: 48px 0 200px 0;
+  text-decoration: underline;
+}
+
+.list__side {
+  width: 20%;
+  min-width: 20%;
+  max-width: 20%;
+  margin-top: -40px;
+  position: sticky;
+  top: 0;
+  align-self: flex-start;
+  padding-top: 48px;
+}
+.list__main {
+  flex-grow: 1;
+  padding-bottom: 40vh;
+}
+
+.types__list-item {
+  position: relative;
+  text-indent: 16px;
+
+  &:not(:last-child) {
+    margin-bottom: 20px;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: calc(50% - 2px / 2);
+    left: 0;
+    width: 8px;
+    height: 2px;
+    background-color: black;
+
+    transition: all 0.3s;
+    transform-origin: left center;
+  }
+}
+.types__list-btn {
+  @include -f-main-b;
+  font-size: 40px;
+  letter-spacing: -1.25px;
+
+  transition: all 0.3s;
+}
+
+.readings {
+  @include -f-main-b;
+  font-size: 80px;
+}
+.readings__list-item {
+  &:not(:last-child) {
+    margin-bottom: 88px;
+  }
+}
+.readings__list-item-link {
+  text-decoration: none;
+  position: relative;
+
+  &:before {
+    content: "→";
+    position: absolute;
+    bottom: 16px;
+    right: -80px;
+
+    opacity: 0;
+    transition: all 0.3s;
+  }
+}
+.readings__list-item-info {
+  font-weight: lighter;
+  font-size: 18px;
+  margin-left: 6px;
+  margin-bottom: 24px;
+
+  & .__info-title {
+    &:not(:last-child) {
+      position: relative;
+      margin-right: 16px;
+
+      &:after {
+        content: "/";
+        position: absolute;
+        right: -11px;
+        top: 1px;
+      }
+    }
+  }
+}
+
+// user events
+.types__list-item {
+  @include hover {
+    opacity: 1;
+    cursor: pointer;
+
+    &:before {
+      transform: scaleX(2);
+      transform-origin: left center;
+      transition: all 0.3s;
+    }
+    & .types__list-btn {
+      transform: translate3d(12px, 0, 0);
+      transition: all 0.3s;
+    }
+  }
+}
+
+.readings__list-item {
+  transition: all 0.3s;
+  & > * {
+    transition: all 0.3s;
+  }
+  @include hover {
+    transform: translate3d(8px, 0, 0);
+
+    & .readings__list-item-link:before {
+      transform: translate3d(24px, 0, 0);
+      opacity: 1;
+      transition: all 0.3s;
+    }
+  }
+}
+
+// states
+.types__list-item {
+  opacity: 0.85;
+  transition: all 0.3s;
+
+  &.-is-active {
+    pointer-events: none;
+    opacity: 1;
+
+    &:before {
+      transform: scaleX(7);
+      transform-origin: left center;
+      transition: all 0.3s;
+    }
+    & .types__list-btn {
+      transform: translate3d(48px, 0, 0);
+    }
+  }
+}
+</style>
