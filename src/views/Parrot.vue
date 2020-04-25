7fcca8672b6c08ca02a4e21400cbed804982143f
vince19972 <vince19972@gmail.com>
2020-04-24 22:38:06 -0400

    [parrot] remove not required component

--- src/views/Parrot.vue
+++ src/views/Parrot.vue
@@ -5,13 +5,11 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import GridSection from "@/components/sections/gridSection/GridSection.vue";
-import ParrotGrid from "@/components/partials/ParrotGrid.vue";
 
 @Component({
   components: {
     GridSection,
-    ParrotGrid
-  }
+  },
 })
-export default class Chicken extends Vue {}
+export default class Parrot extends Vue {}
 </script>
