3ceb6cfd2a0f91c56bf1f307a0ecb2db086c2c6d
vince19972 <vince19972@gmail.com>
2020-04-26 18:36:37 -0400

    [chicken] update correct name files

---
+++ src/components/comps/chicken/Produce.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="produce">
+    <div class="lead">
+      <div class="lead-input -bar"></div>
+      <p class="lead-text -f-grid-condensed" ref="textLead">CHICKEN</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Produce",
+})
+export default class Produce extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/sections/GridSection.scss";
+
+$bar-duration: 1.5s;
+$input-bar-length: 100%;
+$inner-bar-height: 10vh;
+
+.produce {
+  height: 100%;
+  writing-mode: vertical-rl;
+  border-right: calc(#{$gutter-v} / 2) solid black;
+}
+
+.-bar {
+  background-color: black;
+}
+.lead-input.-bar {
+  position: relative;
+  height: $input-bar-length;
+  width: calc(#{$gutter-v} / 1.5);
+  margin-bottom: calc(#{$v-gutter-v} / 6);
+  border-radius: 0 0 calc(#{$gutter-v}* 0.5) calc(#{$gutter-v}* 0.5);
+}
+.lead-input.-bar:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: calc((100% - 15%) / 2);
+  width: 15%;
+  height: $inner-bar-height;
+  background-color: white;
+}
+
+.lead {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.lead-text {
+  font-size: $f-size-lead;
+  line-height: $f-size-lead;
+}
+
+@keyframes moveInputBar {
+  0%,
+  100% {
+    transform: translate3d(0, -100%, 0);
+  }
+  30% {
+    transform: translate3d(0, -1%, 0);
+  }
+  80% {
+    transform: translate3d(0, 0, 0);
+  }
+}
+@keyframes moveLeadText {
+  0%,
+  100% {
+    letter-spacing: normal;
+  }
+  30% {
+    letter-spacing: -1.9rem;
+  }
+  80% {
+    letter-spacing: -2rem;
+  }
+}
+@keyframes moveInnerBar {
+  0%,
+  100% {
+    opacity: 0;
+    top: -100%;
+  }
+  60% {
+    opacity: 1;
+    top: -100%;
+  }
+  79% {
+    opacity: 1;
+    top: calc(100% - #{$inner-bar-height});
+  }
+  80% {
+    opacity: 1;
+    top: 100vh;
+  }
+  81% {
+    opacity: 0;
+  }
+}
+
+.lead-input,
+.lead-text,
+.lead-input.-bar:after {
+  // animation-timing-function: ease-in-out;
+  animation-duration: $bar-duration;
+  animation-iteration-count: infinite;
+}
+.lead-input {
+  animation-name: moveInputBar;
+}
+.lead-text {
+  animation-name: moveLeadText;
+}
+.lead-input.-bar:after {
+  animation-name: moveInnerBar;
+}
+</style>
---
+++ src/components/comps/chicken/Selection.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="selection -f-grid-main">
+    <div class="text-group">
+      <div class="text-row -flex" v-for="r in textCount.row" :key="r + 'row'">
+        <div
+          :class="['text-col', { '-is-useless': isUseless(c) }]"
+          v-for="c in textCount.col"
+          :key="c + 'col'"
+        >
+          <p class="text-col__text">
+            {{ textType(c) }}
+          </p>
+          <div v-if="isUseless(c)" class="text-col__cover"></div>
+        </div>
+      </div>
+    </div>
+    <p class="text -hidden" ref="textHidden">CHICKEN</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { WindowSize as WindowSizeState } from "@/store/types/browser";
+import { TimelineMax, SteppedEase, Back } from "gsap";
+
+@Component({
+  name: "Selection",
+})
+export default class Selection extends Vue {
+  // computed setters
+  get windowSize(): WindowSizeState {
+    return this.$store.getters["browser/windowSize"];
+  }
+  get textCount() {
+    return {
+      row: Math.ceil(this.windowSize.height / this.textValue.height),
+      col: Math.ceil(this.windowSize.width / this.textValue.width) * 4,
+    };
+  }
+
+  // methods
+  isUseless(index: number) {
+    return index % 2 !== 0;
+  }
+  textType(index: number) {
+    return this.isUseless(index) ? "chIcken" : "chicken";
+  }
+  moveTimeline(tl: TimelineMax | any) {
+    const selection = ".selection";
+    const textCol = ".text-col";
+    const text = `${selection} ${textCol}__text`;
+    const $selection = selection;
+    const $textCover = `${textCol}__cover`;
+    const $textColUseless = `${textCol}.-is-useless`;
+    const $text = text;
+    const $textUseless = `${selection} ${textCol}.-is-useless ${textCol}__text`;
+
+    tl.staggerFromTo(
+      $text,
+      0.1,
+      { opacity: 0, y: "-100%" },
+      { opacity: 1, y: "0" },
+      0.01,
+      "hatched"
+    )
+      .to($selection, 0.5, { backgroundColor: "black" }, "hatched+=1")
+      .addLabel("filtering", "+=0")
+      // .to($selection, 0.5, { backgroundColor: "white" }, "filtering+=0.5")
+      .addLabel("filtered", "+=0")
+      .staggerTo($textUseless, 0.1, { color: "red" }, 0.01, "filtered-=0.5")
+      .staggerTo(
+        $textCover,
+        0.05,
+        { width: "100%", ease: SteppedEase.config(4) },
+        {
+          amount: 1,
+          from: "random",
+          ease: Back.easeOut.config(1.7),
+        },
+        "filtered+=0.25"
+      )
+      .addLabel("eliminating", "+=0")
+      .staggerTo($textUseless, 0.1, { opacity: 0 }, 0, "eliminating+=0")
+      .staggerTo($textCover, 0.1, { width: "0" }, 0.01, "eliminating+=1")
+      .staggerTo($textUseless, 0.1, { width: "0" }, 0, "eliminating+=1")
+      .staggerTo(
+        $textColUseless,
+        0.1,
+        { marginRight: 0 },
+        0.01,
+        "eliminating+=1"
+      )
+      .addLabel("eliminated", "+=0")
+      .staggerTo($text, 0.05, { y: "100%" }, 0.01, "eliminated+=0")
+      .staggerTo($text, 0.01, { y: "0", opacity: 0 }, 0.01, "eliminated+=0.05");
+  }
+
+  // data
+  textValue = {
+    width: this.windowSize.width,
+    height: this.windowSize.height,
+  };
+  timeline: TimelineMax = new TimelineMax({ repeat: -1 });
+
+  // life cycle
+  mounted() {
+    // text values
+    const text = this.$refs.textHidden as HTMLElement;
+    this.textValue.width = text.offsetWidth;
+    this.textValue.height = text.offsetHeight;
+  }
+  updated() {
+    // gsap
+    /*
+    // we need to restart the timeline,
+    // bringing it back to step 0 before invalidating/clearing it,
+    // preventing timeline keeps the state before updated.
+    */
+    this.timeline.restart().invalidate();
+    this.timeline.clear();
+    this.moveTimeline(this.timeline);
+    this.timeline.restart();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/sections/GridSection.scss";
+
+.selection {
+  height: 100%;
+  font-size: calc(#{$f-size-lead} * 2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.text-row {
+  &:nth-child(1n) {
+    transform: translate3d(-10vw, 0, 0);
+  }
+  &:nth-child(2n) {
+    transform: translate3d(-20vw, 0, 0);
+  }
+}
+
+.text.-hidden {
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+}
+
+.text-col {
+  position: relative;
+  margin-right: calc(#{$v-gutter} / 2);
+}
+.text-col__text {
+  text-align: center;
+  font-weight: lighter;
+}
+.text-col__cover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: red;
+
+  // animation
+  width: 0;
+}
+</style>
