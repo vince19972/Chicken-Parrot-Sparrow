ceec7029f910d6a433ed5085e0d969b3b47b0596
vince19972 <vince19972@gmail.com>
2020-04-26 13:01:51 -0400

    [BirdsAndRoles] change dotsConnection to new route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import BirdsAndRoles from "../views/BirdsAndRoles.vue";
 import Chicken from "../views/Chicken.vue";
 import Parrot from "../views/Parrot.vue";
 import Sparrow from "../views/Sparrow.vue";
@@ -13,6 +14,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/birds-and-roles",
+    name: "BirdsAndRoles",
+    component: BirdsAndRoles,
   },
   {
     path: "/chicken",
---
+++ src/views/BirdsAndRoles.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="birds-and-roles">
+    <OpeningSection />
+  </div>
+</template>
+
+<script>
+import OpeningSection from "@/components/sections/OpeningSection.vue";
+
+export default {
+  name: "BirdsAndRoles",
+  components: {
+    OpeningSection,
+  },
+};
+</script>
