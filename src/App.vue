26dd50f5c3942d6a62515256a066a4cdbde90fb5
vince19972 <vince19972@gmail.com>
2020-05-02 22:46:51 -0400

    [seo] update seo info

--- src/App.vue
+++ src/App.vue
@@ -64,18 +64,54 @@ import { Meta } from "@/decorator.js";
           property: "og:url",
           content: "https://www.chicken-parrot-sparrow.com/",
         },
+        {
+          key: "og:image",
+          name: "og:image",
+          property: "og:image",
+          content: "./assets/imgs/og.jpg",
+        },
+        {
+          key: "og:description",
+          name: "og:description",
+          property: "og:description",
+          content:
+            "An interactive website features typography animation that inspires audiences to review why and how certain birds in modern urban life aren't living as birds but being objectified as food or products.",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          key: "twitter:site",
+          name: "twitter:site",
+          content: "@vincemingpushao",
+        },
+        {
+          key: "twitter:title",
+          name: "twitter:title",
+          content: "Chicken, Parrot and Sparrow",
+        },
+        {
+          key: "twitter:description",
+          name: "twitter:description",
+          content:
+            "An interactive website features typography animation that inspires audiences to review why and how certain birds in modern urban life aren't living as birds but being objectified as food or products.",
+        },
+        {
+          key: "twitter:creator",
+          name: "twitter:creator",
+          content: "@vincemingpushao",
+        },
+        {
+          key: "twitter:image",
+          name: "twitter:image",
+          content: "./assets/imgs/og.jpg",
+        },
       ],
     };
   },
 })
 export default class App extends Vue {
-  // @Meta
-  // metaInfo() {
-  //   return {
-  //     title: "Chicken, Parrot and Sparrow",
-  //   };
-  // }
-
   // style getters
   get stateClass() {
     return this.$store.getters["isMenuOpened"] ? "-is-frozen" : "";
